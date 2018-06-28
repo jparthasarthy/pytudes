9e70bdd9b675bde68289f44a9b3090b6da657e6d
Jparthas <jparthasarthy@gmail.com>
2018-06-27 21:31:09 -0400

    first commit

---
+++ coin_flip.py
@@ -0,0 +1,19 @@
+"""My solution to the coin flip puzzle (https://github.com/norvig/pytudes/blob/master/ipynb/Coin%20Flip.ipynb).
+Explanation of data structures:
+Coins: Representation of coin structure. Implemented as a string (e.g. 'HHHT')
+State: Representation of belief state: A set of coins, representing possible states coins are in.
+Move: A move is a set of positions to flip (e.g. {0, 1})
+Position: A tuple of (List[Move], State).
+Possible Moves: """
+
+def shortest_path_search():
+    
+
+
+def flip_coins(coins):
+    """Applies the """
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ uber_interview.py
@@ -0,0 +1,23 @@
+"""Input: [1,2,3,4,5]
+Output: [120, 60, 40, 30, 24]
+"""
+
+
+def product(iterable):
+    i = 1
+    for n in iterable:
+        i *= n
+    return i
+
+
+def new_array(input):
+    new = []
+    n = len(input)
+    x = product(input)
+    for i in range(n):
+        new.append(x / input[i])
+    return new
+
+
+if __name__ == "__main__":
+    print(new_array([1, 2, 3, 4, 5]))
